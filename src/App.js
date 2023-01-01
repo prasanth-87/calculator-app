4c144ce7e84ecb0e9d1050e94b2e8ee58e0e5789
Prasanth Reddy <prasanthreddy787@gmail.com>
2023-01-01 12:02:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React from "react"
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [input,setInput]=useState('')
+  const [result,setResult]=useState(0)
+  const handler=(e)=>{
+    setInput(e.target.value.replace(/\D/g, ''));
+  }
+  const handleClick=()=>{
+    setResult(eval(input));
+    setInput('');
+  }
+  const clearHnadle=()=>{
+    setInput("")
+    setResult(0)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h5>Calculator:{result}</h5>
+      <div className="App">
+      <center >
+        <div className="input">
+          <input className="rounded mt-2 p-1" type="text"  value={input} onChange={handler}/>
+        </div>
+        <div>
+        <button onClick={()=>setInput(input+"(")}>(</button>
+          <button onClick={()=>setInput(input+")")}>)</button>
+          <button onClick={()=>setInput(input+"%")}>%</button>
+          <button onClick={clearHnadle}>CE</button> <br/>
+
+          <button onClick={()=>setInput(input+"7")}>7</button>
+          <button onClick={()=>setInput(input+"8")}>8</button>
+          <button onClick={()=>setInput(input+"9")}>9</button>
+          <button onClick={()=>setInput(input+"/")}>/</button> <br/>
+
+          <button onClick={()=>setInput(input+"4")}>4</button>
+          <button onClick={()=>setInput(input+"5")}>5</button>
+          <button onClick={()=>setInput(input+"6")}>6</button>
+          <button onClick={()=>setInput(input+"*")}>*</button> <br/>
+
+          <button onClick={()=>setInput(input+"1")}>1</button>
+          <button onClick={()=>setInput(input+"2")}>2</button>
+          <button onClick={()=>setInput(input+"3")}>3</button>
+          <button onClick={()=>setInput(input+"-")}>-</button> <br/>
+
+          <button onClick={()=>setInput(input+"0")}>0</button>
+          <button onClick={()=>setInput(input+".")}>.</button>
+          <button onClick={handleClick} >=</button>
+          <button onClick={()=>setInput(input+"+")}>+</button>
+        </div>
+      </center>
+    </div>
     </div>
   );
 }
